cc6a26d9eeaa1144d77fb669d686a1d13392c730
jerico1990 <cesar.gago.egocheaga@gmail.com>
2016-05-19 15:59:57 -0500

    19052016

--- views/layouts/minedu.php
+++ views/layouts/minedu.php
@@ -35,6 +35,8 @@ use yii\widgets\Breadcrumbs;
 	
 </head>
 <body>
+
+<?php $this->beginBody() ?>
     <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PZX7QM"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -44,7 +46,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PZX7QM');</script>
 <!-- End Google Tag Manager -->
-<?php $this->beginBody() ?>
 <header>
 	<div class="bar_yellow"></div>
 
