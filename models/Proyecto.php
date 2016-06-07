1f00dd0432532c46e342ac3b06037b7ce0e0217b
jerico1990 <cesar.gago.egocheaga@gmail.com>
2016-06-07 16:24:59 -0500

    07062016

--- models/Proyecto.php
+++ models/Proyecto.php
@@ -178,7 +178,9 @@ class Proyecto extends \yii\db\ActiveRecord
         
         //total_equipos province   total_alumnos  district  total_equipos_nofinalizado latitude  total_alumnos_nofinalizado longitud
         $query = new Query;
-        $query
+        if($region)
+        {
+            $query
             ->select(['
                         p.id,
                         p.asunto_id,
@@ -200,6 +202,32 @@ class Proyecto extends \yii\db\ActiveRecord
             ->where('u.department_id=:department_id',[':department_id'=>$region])
             ->groupBy('p.id,p.titulo')
             ->orderBy($sort);
+        }
+        else
+        {
+            $query
+            ->select(['
+                        p.id,
+                        p.asunto_id,
+                        p.titulo, 
+                        COUNT( i.estudiante_id ) AS total_integrantes,
+                        IF((select count(DISTINCT integrante.estudiante_id) from foro inner join foro_comentario on foro_comentario.foro_id=foro.id  inner join usuario on usuario.id=foro_comentario.user_id inner join estudiante on estudiante.id=usuario.estudiante_id inner join integrante on integrante.estudiante_id=estudiante.id inner join equipo on equipo.id=integrante.equipo_id where foro.id=2 and estudiante.grado!=6 and p.equipo_id=equipo.id )=(COUNT( i.estudiante_id )-1),1,0) as foro_abierto,
+                        IF((select count(DISTINCT integrante.estudiante_id) from foro inner join foro_comentario on foro_comentario.foro_id=foro.id  inner join usuario on usuario.id=foro_comentario.user_id inner join estudiante on estudiante.id=usuario.estudiante_id inner join integrante on integrante.estudiante_id=estudiante.id inner join equipo on equipo.id=integrante.equipo_id where foro.asunto_id=p.asunto_id and estudiante.grado!=6 and p.equipo_id=equipo.id )=(COUNT( i.estudiante_id )-1),1,0) as foro_asunto,
+                        IF((SELECT COUNT(video.proyecto_id) FROM video WHERE video.proyecto_id = p.id AND TRIM( video.ruta ) IS NOT NULL and TRIM( video.ruta )!="") =1, 1, 0 ) AS video_check,
+                        IF( ( SELECT COUNT( reflexion.proyecto_id ) FROM reflexion WHERE reflexion.proyecto_id = p.id AND TRIM( reflexion.p1 ) IS NOT NULL and TRIM( reflexion.p1 )!="" AND TRIM( reflexion.p2 ) IS NOT NULL and TRIM( reflexion.p2 )!="" AND TRIM( reflexion.p3 ) IS NOT NULL and TRIM( reflexion.p3 )!="") =1, 1, 0 ) AS reflexion_check,
+                        IF(trim(p.proyecto_archivo)!="",1,0) as archivo_proyecto_check,
+                        IF(e.etapa=1,1,0) as proyecto_finalizado
+                      '])
+            ->from('proyecto p')
+            ->innerJoin('equipo e','e.id = p.equipo_id')
+            ->innerJoin('integrante i','i.equipo_id = e.id')
+            ->innerJoin('estudiante es','es.id=i.estudiante_id')
+            ->innerJoin('institucion ins','ins.id=es.institucion_id')
+            ->innerJoin('ubigeo u','u.district_id=ins.ubigeo_id')
+            ->groupBy('p.id,p.titulo')
+            ->orderBy($sort);
+        }
+        
             
         $result = Yii::$app->tools->Pagination($query,10);
         
